9f58adc09db8a12d190cc604837058084f9bc83e
Sebastian Fichtner <sebastian@codeface.io>
2025-09-03 15:11:35 +0200

    Extract .env loading to separate script file

---
+++ scripts/set_permissions_and_load_env.sh
@@ -0,0 +1,21 @@
+#!/bin/zsh
+# This script sets safe file permissions and loads the .env file
+
+# Set safe permissions for project directory
+echo "🔒 Setting safe permissions for project directory ..."
+chmod 700 "$root_dir" # Only owner can access project
+
+# Set safe permissions for .env file and load it
+echo "🔒 Setting safe permissions for .env file and loading it ..."
+
+env_file="$root_dir/.env"
+
+if [[ -f "$env_file" ]]; then
+    chmod 600 "$env_file" # Only owner can read/write .env file
+    set -a                # Automatically export all variables
+    source "$env_file"    # Load environment variables from .env file
+    set +a                # Turn off auto-export
+else
+    echo "⚠️  No .env file found. Copy .env.example to .env and customize it."
+    exit 1
+fi
--- update.sh
+++ update.sh
@@ -1,31 +1,14 @@
 #!/bin/zsh
 # This script updates (sets up) this Mac
 
+# Prepare
 set -e  # Exit on any error
-absolute_dir="$(realpath "$(dirname "$0")")"
+root_dir="$(realpath "$(dirname "$0")")"
+source "$root_dir/scripts/set_permissions_and_load_env.sh"
 
-# Secure project directory
-echo "🔒 Securing project directory ..."
-chmod 700 "$absolute_dir" # Only owner can access project
-
-# Secure .env file and load environment variables from it
-echo "🔒 Securing and loading .env file ..."
-
-env_file="$absolute_dir/.env"
-
-if [[ -f "$env_file" ]]; then
-    chmod 600 "$env_file" # Only owner can read/write .env file
-    set -a                # Automatically export all variables
-    source "$env_file"    # Load environment variables from .env file
-    set +a                # Turn off auto-export
-else
-    echo "⚠️  No .env file found. Copy .env.example to .env and customize it."
-    exit 1
-fi
-
-# Run the actual scripts
-"$absolute_dir/scripts/update_homebrew_stack.sh"
-"$absolute_dir/scripts/update_zshrc_file.sh"
-"$absolute_dir/scripts/update_other_software.sh"
+# Run the scripts
+"$root_dir/scripts/update_homebrew_stack.sh"
+"$root_dir/scripts/update_zshrc_file.sh"
+"$root_dir/scripts/update_other_software.sh"
 
 echo "✅ Did update (set up) this Mac"
