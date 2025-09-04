c8d2c83bbbd60c57beb62a08168b7b0d5efe022a
Sebastian Fichtner <sebastian@codeface.io>
2025-09-04 21:46:30 +0200

    Make shell personalization script file optional

--- scripts/sourced_in_zshrc/sourced_in_zshrc.sh
+++ scripts/sourced_in_zshrc/sourced_in_zshrc.sh
@@ -3,6 +3,9 @@
 # Get script directory (in a way that works when sourced rather than executed)
 SCRIPT_DIR="$(dirname "${(%):-%x}")"
 
-source "$SCRIPT_DIR/customize_the_shell.sh"
-source "$SCRIPT_DIR/personalize_the_shell.sh"
 source "$SCRIPT_DIR/setup_cli_tools.sh"
+source "$SCRIPT_DIR/customize_the_shell.sh"
+
+if [[ -f "$SCRIPT_DIR/personalize_the_shell.sh" ]]; then
+    source "$SCRIPT_DIR/personalize_the_shell.sh"
+fi
