74d0bdfaf1eebed4b6739821ef52a16c8baeee60
Sebastian Fichtner <sebastian@codeface.io>
2025-09-06 18:43:04 +0200

    Edit files: update_other_software.sh

--- scripts/update_other_software.sh
+++ scripts/update_other_software.sh
@@ -8,7 +8,7 @@ set -u  # Treat unset variables as error
 
 source "$MAC_STACK_ROOT/scripts/sourced_in_zshrc/customize_the_shell.sh" # load `python-latest` alias
 latest_python_version="$(python-latest)"
-echo "🐍 Installing latest python ($latest_python_version) and pip ..."
+echo "🐍 Installing latest python ($latest_python_version) and updating pip ..."
 pyenv install --skip-existing "$latest_python_version"
 pyenv global "$latest_python_version"
 python -m pip install --upgrade pip
