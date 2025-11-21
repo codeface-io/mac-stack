cf3dd82c5684fb628a874a96b982fb73bf8e4ca2
Sebastian Fichtner <sebastian@codeface.io>
2025-11-21 15:54:12 +0100

    Let brewfile-clip do proper uninstalls if possible

--- brewfile_clip.sh
+++ brewfile_clip.sh
@@ -2,7 +2,7 @@
 # This script enforces the philosophy that every Homebrew package used must be declared explicitly in the Brewfile.
 
 # It uninstalls:
-# 1) brew packages that are not (yet) declared in the Brewfile
+# 1) brew packages that are not (yet) declared in the Brewfile, does proper deep uninstalls for casks (apps) beyond just deleting them from Applications folder
 # 2) brew packages that are orphaned dependencies (Orphaned means they were once installed as dependencies but are not depended upon anymore. Brew intentionally tracks and keeps such orphans.)
 # 3) unused old package versions, caches
 # 4) Cask installers. This disables offline reinstalls (virtually never needed) but frees up a lot of disk space.
@@ -20,7 +20,7 @@ fi
 
 # Proceed with uninstallations
 echo "🧹 Uninstalling ..."
-brew bundle cleanup --force --file "$BREWFILE_PATH" # (1)
+brew bundle cleanup --force --zap --file "$BREWFILE_PATH" # (1)
 brew autoremove # (2)
 brew cleanup # (3)
 find /opt/homebrew/Caskroom -type d -name '.metadata' -prune -o -type f -delete # (4)
